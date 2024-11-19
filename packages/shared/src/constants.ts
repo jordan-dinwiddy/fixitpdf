d49b7c6cf43a26a5d23d486b4ea5aaada289442e
Jordan Dinwiddy <jordan.dinwiddy@gmail.com>
2024-11-19 01:36:42 -0800

    Fix stripe price

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -11,5 +11,5 @@ export const PROD_PURCHASE_OPTIONS: PurchaseOption[] = [
   { id: '1', priceId: 'price_1QMPNkRqb2FjfvrJOgUxkNWh', credits: 5, price: 5, tagline: "Perfect to try things out" },
   { id: '2', priceId: 'price_1QMPO4Rqb2FjfvrJtd7oSyca', credits: 15, price: 10, tagline: "Save 33%" },
   { id: '3', priceId: 'price_1QMPOWRqb2FjfvrJW7ecKSJ3', credits: 35, price: 20, tagline: "Save 43%" },
-  { id: '4', priceId: 'price_1QMPOoRqb2FjfvrJk9e9Mmlw', credits: 100, price: 50, tagline: "Best value! Save 50%" },
+  { id: '4', priceId: 'price_1QMnqORqb2FjfvrJfqQ9Ucum', credits: 100, price: 50, tagline: "Best value! Save 50%" },
 ];
